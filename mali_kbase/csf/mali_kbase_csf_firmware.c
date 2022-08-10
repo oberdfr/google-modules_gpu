a6b902d410c289a9e0e6bcb83cb6e3ff9234e1b0
Jesse Hall <jessehall@google.com>
2022-08-10 16:27:44 -0700

    csf/firmware: clean fw interface before reading
    Bug: 239506823
    Test: boot 3 times and check that interface parsing succeeds with retry_count==0
    Signed-off-by: Jesse Hall <jessehall@google.com>
    Change-Id: Id077388627d62208eee10361447c6531da2f8a9f

--- mali_kbase/csf/mali_kbase_csf_firmware.c
+++ mali_kbase/csf/mali_kbase_csf_firmware.c
@@ -2148,6 +2148,19 @@ int kbase_csf_firmware_init(struct kbase_device *kbdev)
 
 	boot_csf_firmware(kbdev);
 
+	{
+		struct kbase_csf_firmware_interface *interface = kbdev->csf.shared_interface;
+		const u32 num_pages = interface->num_pages;
+		u32 i;
+
+		dev_dbg(kbdev->dev, "invalidating %u fw interface pages", num_pages);
+		for (i = 0; i < num_pages; i++) {
+			struct page *pg = as_page(interface->phys[i]);
+			kbase_sync_single_for_cpu(kbdev, kbase_dma_addr(pg), PAGE_SIZE,
+						  DMA_BIDIRECTIONAL);
+		}
+	}
+
 	/* Remove this workaround once b/239506823 is Fixed */
 	while (retry_count++ < MAX_TRIES_PARSE_FW_GLOBAL_INT)
 	{
